0fd9be2eafcc21fc83d91f29d3247e119ecaaaaa
Diogo Gomes <dgomes@ua.pt>
2021-02-26 21:47:45 +0000

    refactor

---
+++ custom_components/hasp-lvgl/consts.py
@@ -0,0 +1,11 @@
+"""Constants for HASP Open Hardware edition custom component."""
+
+TOGGLE=["off", "on"]
+DOMAIN = "hasp-lvgl"
+CONF_OBJID = "obj"
+CONF_ENTITY = "entity"
+CONF_TOPIC = "topic"
+CONF_PAGE_ENTITY = "page_entity"
+CONF_OBJECTS = "objects"
+
+DEFAULT_TOPIC = "hasp"
