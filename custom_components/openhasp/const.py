8d7acda25aa5f9079f7eea1a74cc28bdc512c273
Diogo Gomes <dgomes@ua.pt>
2023-01-29 17:35:19 +0000

    add optional image resize to fit dimensions

--- custom_components/openhasp/const.py
+++ custom_components/openhasp/const.py
@@ -80,7 +80,7 @@ HASP_ONLINE = "online"
 HASP_OFFLINE = "offline"
 HASP_LWT = (HASP_ONLINE, HASP_OFFLINE)
 
-ATTR_FORCE_FITSCREEN = "fit screen"
+ATTR_FORCE_FITSCREEN = "fit_screen"
 ATTR_PAGE = "page"
 ATTR_CURRENT_DIM = "dim"
 ATTR_IDLE = "idle"
