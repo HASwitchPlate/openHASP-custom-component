f25c3d235e0430e8f1b728270bfc2c5d1b878eec
xNUTx <nut@fun-industries.nl>
2024-08-08 16:47:37 +0200

    if statement reverted to include in a different PR

--- custom_components/openhasp/__init__.py
+++ custom_components/openhasp/__init__.py
@@ -556,7 +556,7 @@ class SwitchPlate(RestoreEntity):
         if self._statusupdate:
             num_pages = self._statusupdate[HASP_NUM_PAGES]
 
-            if page <= 0 or int(page) > int(num_pages):
+            if page <= 0 or page > num_pages:
                 _LOGGER.error(
                     "Can't change to %s, available pages are 1 to %s", page, num_pages
                 )
